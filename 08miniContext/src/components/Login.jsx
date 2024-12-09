914f06b49b1f0318aa128875e2afb5bd7b971294
rohan <sharmarohan2507@gmail.com>
2024-12-10 02:36:50 +0530

    context almost complete

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useState,useContext } from 'react'
+import userContext from '../context/UserContext'
 
 export default function Login() {
         
     const [username,setUsername]=useState('')
     const [pass,setPass]=useState('')
+
+    const {setUser}=useContext(userContext)
 
     const handleSubmit=(e)=>{
         //prevent autorefresh
